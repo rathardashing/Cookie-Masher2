69b10e5caa92de3b41f09b46b2310fec27a3c0ef
rathardashing <73247196+rathardashing@users.noreply.github.com>
2020-10-22 03:14:11 -0400

    Update Sim.js

--- src/Sim.js
+++ src/Sim.js
@@ -186,7 +186,8 @@ CM.Sim.CalculateGains = function() {
 	if (CM.Sim.Has('A lump of coal')) mult *= 1.01;
 	if (CM.Sim.Has('An itchy sweater')) mult *= 1.01;
 	if (CM.Sim.Has('Santa\'s dominion')) mult *= 1.2;
-
+	if (CM.Sim.Has('Dragon scale')) mult *= 1.03;
+	
 	var buildMult = 1;
 	if (Game.hasGod) {
 		var godLvl = Game.hasGod('asceticism');
